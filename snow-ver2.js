b072ba920cb9c694a96a2bb6a2ace8e01450de18
aka.あかつき <Aka-Akatsuki@users.noreply.github.com>
2019-02-11 19:48:10 +0900

    動きを修正
    雪の大きさをランダムにする
    雪の移動距離／方向は一定回数同じ値を繰り返す

--- snow-ver2.js
+++ snow-ver2.js
@@ -18,7 +18,7 @@
    snowR = 5;
    snowspeed = 3;
    
-   MaxNum = 1000;
+   MaxNum = 500;
    
    for (let i = 0; i < MaxNum; i++) {   
      snow[i] = new objSnow();
@@ -52,8 +52,11 @@
       this.speed = 1;
       this.sy = 0;
       this.sx = random(640);
-      this.fai = 2;
+      this.currentsx = 0;
+      this.currentsy = 0;
+      this.fai = random(2.5);
       this.wait = random(1000);
+      this.stepcount = 0;
    }
   
 
@@ -61,8 +64,20 @@
    snowdraw() {
       if (this.wait <= 0) {
          ellipse(this.sx, this.sy, this.fai, this.fai);
-         this.sy += random(this.speed);
-         this.sx += random(-1*this.speed, this.speed);
+
+         // this.sy += random(this.speed);
+         // this.sx += random(-1*this.speed, this.speed);
+         
+         if(this.stepcount <= 0) {
+            this.currentsy = random(this.speed);
+            this.currentsx = random(-1*this.speed, this.speed);
+            this.stepcount = 30;
+         }
+
+         this.sy += this.currentsy;
+         this.sx += this.currentsx;
+         this.stepcount -= 1;
+         
          if(this.sy > 480) {
             this.sy = 0;
          }
