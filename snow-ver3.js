040c4a2979cbd16e71a87d5f3bc210d26316bb55
aka.あかつき <Aka-Akatsuki@users.noreply.github.com>
2019-03-13 22:44:16 +0900

    コメントを追加
    不要な変数を削除
    コメントを追加する

--- snow-ver3.js
+++ snow-ver3.js
@@ -1,16 +1,13 @@
-// ver.1.1  Test Coding 
+// ver.3 Coding 
 
- var snowcolor;
- var snowR;
- var y;
- var MaxNum;
- var baseup;
+ var snowcolor;   //雪の色
+ var MaxNum;      //雪の数
  var  strtime;
  var  resettime;
  var  restbuf;
  
- let snow = [];
- var arypoint = [];
+ let snow = [];        //雪オブジェクトの配列
+ var arypoint = [];    //積もる雪の高さを画面1ドットづつ記録
 
  function setup() {
    //size(screen.width, screen.height);
@@ -19,20 +16,18 @@
    background(0,0,100);
      
    snowcolor = 200;
-   y = 0;
-   snowR = 5;
    snowspeed = 1;
    
    MaxNum = 500;
-   baseup = 0;
    resettime = 200;
    restbuf = 20;
-   strtime = 0;
    
+   //雪オブジェクト作成
    for (let i = 0; i < MaxNum; i++) {   
      snow[i] = new objSnow();
    }
  
+   //雪が積もる高さの初期化
    for (let i = 0; i < width ; i++) {   
      arypoint[i] = 0;
    }
@@ -40,27 +35,14 @@
 }
 
  function draw() {
-
-    if (strtime >= resettime) {
-       strtime = 0;
-       if (millis() > 15000) {
-          baseup += 1;
-       }
-    }
-    strtime += 1;
   
-    // chage background writeing
+    // chage background writein
     background(0,0,100);
-    let vy;
-    vy = height - baseup;
     
-    //push();
     stroke(0,0,100);
     fill(0,0,100);
-    //rect(0,0,width,height - baseup);
-    //pop();
      
-/*
+/*  デバック用のコメント出力
     fill(255);
     text(str(strtime),100,100);
     text(str(baseup),200,100);
@@ -72,12 +54,14 @@
   
      
      //ellipse(100,y,snowR,snowR);
-     y+=snowspeed;
+     //y+=snowspeed;
      
+    //雪が降る  ゆきオブジェクトの描写
     for (let i = 0; i < MaxNum; i++) {   
        snow[i].snowdraw();
     }
      
+    //雪が積もる  雪の高さのY座標を計算する
     beginShape();
     vertex(0,height);
     for (let x = 0; x < width ; x++) {   
@@ -89,8 +73,7 @@
     
 }
 
-
-
+ //雪オブジェクト
  class objSnow {
 
    constructor () {
@@ -101,11 +84,10 @@
       this.currentsy = 0;
       this.fai = random(2.5);
       this.wait = random(1000);
-      this.stepcount = 0;
+      this.stepcount = 0;        //指定した回数毎に移動方向を変更する
    }
   
-
-
+   //雪の描写
    snowdraw() {
       if (this.wait <= 0) {
          ellipse(this.sx, this.sy, this.fai, this.fai);
@@ -124,12 +106,8 @@
          this.stepcount -= 1;
          
 
-         if (this.sx < 0) { 
-            this.sx = width / 2;
-         }
-         if (this.sx > width) { 
-            this.sx = width / 2;
-         }
+         if (this.sx < 0) { this.sx = width / 2; }
+         if (this.sx > width) { this.sx = width / 2; }
          
          if (this.sy >= height) {
              this.sy = 0;
@@ -141,10 +119,6 @@
              }
              //text(str(arypoint[int(this.sx)]),200,400);
          }
-         
-
-         
-
          
       }
       else {
